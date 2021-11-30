c97fb75a81d93bd2ddde228ddcb7497bdf994b41
Josh Friese <josh.friese@gmail.com>
2021-11-30 13:46:40 -0500

    Update products.component.ts

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -31,17 +31,17 @@ export class ProductsComponent implements OnInit {
     parsedId = product["_id"];
     product["id"] = parsedId;
     product["url"] = url;
-    console.log(product);
-    this.http
-      .post(
-        `https://hoboseafood.netlify.com/.netlify/functions/postJsonOutput?id=` + parsedId + `` ,
-        product,
-        this._options
-      )
-      .subscribe(res => {
-        console.log(res);
-      });
-    // Snipcart needs to find parsedProducts.
+//     console.log(product);
+//     this.http
+//       .post(
+//         `https://hoboseafood.netlify.com/.netlify/functions/postJsonOutput?id=` + parsedId + `` ,
+//         product,
+//         this._options
+//       )
+//       .subscribe(res => {
+//         console.log(res);
+//       });
+//     // Snipcart needs to find parsedProducts.
   }
 
   ngOnInit(): void {
